82f82281a0c08beaa21854e45c5b1ea6be9c37a0
alexander.biryukov <alexander.biryukov@p2p.org>
2025-01-23 13:29:20 +0400

    add P2pEigenLayerModule

---
+++ src/P2pEigenLayerModule.sol
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: 2025 P2P Validator <info@p2p.org>
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.27;
+
+import "./@safe/common/Enum.sol";
+
+interface ISafe {
+    /// @dev Allows a Module to execute a Safe transaction without any further confirmations.
+    /// @param to Destination address of module transaction.
+    /// @param value Ether value of module transaction.
+    /// @param data Data payload of module transaction.
+    /// @param operation Operation type of module transaction.
+    function execTransactionFromModule(
+        address to,
+        uint256 value,
+        bytes calldata data,
+        Enum.Operation operation
+    ) external returns (bool success);
+}
+
+contract P2pEigenLayerModule {
+
+}
